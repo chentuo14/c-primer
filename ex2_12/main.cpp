1864fe5c13f27721d1eea935609a28d3e6106406
chentuo <ch122633@126.com>
2017-10-18 11:28:13 +0800

    practice 2.12

---
+++ ex2_12/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    int double = 3.14;
+    int _;
+//    int catchc-22;
+//    int 1_or_2 = 1;
+    double Double = 3.14;
+    return 0;
+}
