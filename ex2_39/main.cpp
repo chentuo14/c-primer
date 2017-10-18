2361890a235a4e9f840764aa8b96ad6391be6b47
chentuo <ch122633@126.com>
2017-10-18 15:18:20 +0800

    practise 2.39

---
+++ ex2_39/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct Foo { };
+
+struct Sales_data {
+    string name;
+    int number;
+    string productName;
+    string date;
+};
+
+int main()
+{
+
+    return 0;
+}
