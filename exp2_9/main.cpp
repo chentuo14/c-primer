3352c910a0edad69cef8d2332945aef156ec9fb9
chentuo <ch122633@126.com>
2017-10-18 11:14:32 +0800

    practice 2.9

---
+++ exp2_9/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    std::cin>>int input_value;    //int
+//    int i = {3.14};               //int
+//    double salary = wage = 9999.99; //wage no type
+    int i = 3.14;                   //ok
+
+    return 0;
+}
