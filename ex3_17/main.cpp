ee7fb02b9f4f4dd50add44d081183f25726dc2fd
chentuo <ch122633@126.com>
2017-10-19 14:22:34 +0800

    从cin读入一组词并把他们存入一个vector对象，然后设法把所有词都改写为大写形式。输出改变后的结果，每个词占一行

---
+++ ex3_17/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s1;
+    vector<string> arryStr;
+    unsigned int i = 0;
+
+    while(cin>>s1 && i<10)              //输入10次
+    {
+        arryStr.push_back(s1);
+        i++;
+    }
+
+    for(unsigned int i=0;i<arryStr.size();i++)      //提升为大写
+    {
+        for(int j=0;j<arryStr[i].length();j++)
+        {
+            arryStr[i][j] = toupper(arryStr[i][j]);
+        }
+    }
+
+    for(int i = 0;i<arryStr.size();i++)         //输出
+    {
+        cout<<arryStr[i];
+    }
+
+    return 0;
+}
