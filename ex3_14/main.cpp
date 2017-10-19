184b217dbf854e51ffeb1beb05ee71e6df4253b5
chentuo <ch122633@126.com>
2017-10-19 13:03:27 +0800

    编写一段程序，用cin读入一组整数并把他们存入一个vector对象

---
+++ ex3_14/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    vector<int> arry;
+    while(cin>>num)
+        arry.push_back(num);
+
+    return 0;
+}
