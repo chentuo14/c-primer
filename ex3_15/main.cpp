d8169f7ed5b380dac21b5bbb2525bdd8ec78eed0
chentuo <ch122633@126.com>
2017-10-19 13:05:39 +0800

    改写3.14，这次读入的是字符串

---
+++ ex3_15/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string s1;
+    vector<string> arryStr;
+    while(cin>>s1)
+        arryStr.push_back(s1);
+
+    return 0;
+}
