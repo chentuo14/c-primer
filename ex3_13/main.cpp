e6f414174caeff37d274235e943cf6fd6a73b7d8
chentuo <ch122633@126.com>
2017-10-19 13:43:39 +0800

    my result

--- ex3_13/main.cpp
+++ ex3_13/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    vector<int> v1;
-    vector<int> v2(10);
-    vector<int> v3(10, 42);
-    vector<int> v4{10};
-    vector<int> v5{10, 42};
-    vector<string> v6{10};
-    vector<string> v7{10, "hi"};
+    vector<int> v1;                     //1个，空
+    vector<int> v2(10);                 //10,空
+    vector<int> v3(10, 42);             //10, 都是42
+    vector<int> v4{10};                 //1个，10
+    vector<int> v5{10, 42};             //2个，10、42
+    vector<string> v6{10};              //1个，10
+    vector<string> v7{10, "hi"};        //2个，10、hi
 
     return 0;
 }
