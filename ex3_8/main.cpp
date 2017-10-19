1b9ba675dc96ff9145769c2c780d7ff625675ded
chentuo <ch122633@126.com>
2017-10-19 10:53:12 +0800

    while循环的写法

--- ex3_8/main.cpp
+++ ex3_8/main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 int main()
 {
     string s1 = "sjdsljdlasjda";
-    for(decltype(s1.size()) i=0;i<s1.size();i++)
+//    for(decltype(s1.size()) i=0;i<s1.size();i++)
+//        s1[i] = 'X';
+    decltype(s1.size()) i=0;
+    while(i<s1.size())
+    {
         s1[i] = 'X';
+        i++;
+    }
     cout << s1 << endl;
     return 0;
 }
