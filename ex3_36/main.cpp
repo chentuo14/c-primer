45d3583cf5b45c51447cc508644a78e60abde297
chentuo <ch122633@126.com>
2017-10-20 16:06:24 +0800

    add vector version

--- ex3_36/main.cpp
+++ ex3_36/main.cpp
@@ -7,17 +7,24 @@ int main()
 {
     int a[10] = {0,1,2,3,4,5,6,7,8,9};
     int b[10] = {0,1,2,3,4,5,6,7,8,9};
+    vector<int> v1(a, a+10);
+    vector<int> v2(b, b+10);
     int flag = 0;
 
-    for(int i=0;i<10;i++)
-    {
-        if(a[i]!=b[i])
-        {
-            flag = 1;
-            break;
-        }
-    }
-    if(!flag)
+//    for(int i=0;i<10;i++)
+//    {
+//        if(a[i]!=b[i])
+//        {
+//            flag = 1;
+//            break;
+//        }
+//    }
+//    if(!flag)
+//        cout<<"Equal"<<endl;
+//    else
+//        cout<<"not equal"<<endl;
+
+    if(v1 == v2)
         cout<<"Equal"<<endl;
     else
         cout<<"not equal"<<endl;
