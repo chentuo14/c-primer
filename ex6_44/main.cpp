a4342e36e9944e5f06d6634de32af7735ba68591
chentuo <ch122633@126.com>
2017-10-21 09:38:18 +0800

    将6.2.2节的isShorter函数改写成内联函数

---
+++ ex6_44/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+inline bool isShorter(const string  &s1, const string &s2)
+{
+    return s1.size() < s2.size();
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
