8ddee68cf8b334a7f90b0919c9071be7f9d8e6de
chentuo <ch122633@126.com>
2017-10-30 09:11:51 +0800

    7.33如果我们给Screen添加一个如下所示的size成员将发生什么情况？如果出现了问题，请尝试修改它

---
+++ ex7_33/friend.h
@@ -0,0 +1,85 @@
+#ifndef FRIEND_H
+#define FRIEND_H
+
+#include <iostream>
+#include <vector>
+
+class Screen;
+
+class Window_mgr {
+public:
+    using ScreenIndex = std::vector<Screen>::size_type;
+    void clear(ScreenIndex);
+private:
+    std::vector<Screen> screens;
+};
+
+
+class Screen {
+    friend void Window_mgr::clear(ScreenIndex i);
+public:
+    using pos = std::string::size_type;
+
+    Screen() = default;
+    Screen(pos ht, pos wd) : height(ht), width(wd), contents(ht * wd, ' ') {} // 2
+    Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c)
+    {
+    } // 3
+
+    char get() const { return contents[cursor]; }
+    char get(pos r, pos c) const { return contents[r * width + c]; }
+    inline Screen& move(pos r, pos c);
+    inline Screen& set(char c);
+    inline Screen& set(pos r, pos c, char ch);
+    pos size() const;
+
+    const Screen& display(std::ostream& os) const
+    {
+        do_display(os);
+        return *this;
+    }
+    Screen& display(std::ostream& os)
+    {
+        do_display(os);
+        return *this;
+    }
+
+    void do_display(std::ostream& os) const { os << contents; }
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    std::string contents;
+};
+
+void Window_mgr::clear(ScreenIndex i)
+{
+    if(i>=screens.size()) return;
+    Screen& s = screens[i];
+    s.contents = std::string(s.height*s.width, ' ');
+}
+
+
+inline Screen& Screen::move(pos r, pos c)
+{
+    cursor = r * width + c;
+    return *this;
+}
+
+inline Screen& Screen::set(char c)
+{
+    contents[cursor] = c;
+    return *this;
+}
+
+inline Screen& Screen::set(pos r, pos c, char ch)
+{
+    contents[r * width + c] = ch;
+    return *this;
+}
+
+Screen::pos Screen::size() const        //这里的pos需要加上作用域
+{
+    return height*width;
+}
+
+
+#endif // FRIEND_H
