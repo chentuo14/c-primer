f15e69c4d4369c578292fea04f25dc08785cc071
chentuo14 <ch122633@126.com>
2017-11-05 14:24:11 +0800

    9.51设计一个类，它有三个unsigned成员，分别表示年、月和日。为其编写构造函数，接受一个表示日期的string参数。你的构造函数应该能处 理不同数据格式，如January 1,1900、1/1/1990、Jan 1 1900

--- ex9_51/data.h
+++ ex9_51/data.h
@@ -9,6 +9,7 @@ using namespace std;
 
 class date {
 public:
+    friend ostream& operator<<(ostream&, const date&);
     date() = default;
     date(string &ds);
 
@@ -27,4 +28,91 @@ const string month_abbr[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
                              "Aug", "Sep", "Oct", "Nov", "Dec"};
 const int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+inline int get_month(string &ds, int &end_of_month)
+{
+    int i,j;
+    for(i=0;i<12;++i) {
+        for(j=0;j<month_abbr[i].size();++j)
+            if(ds[j] != month_abbr[i][j])
+                break;
+        if(j==month_abbr[i].size())
+            break;
+    }
+
+    if(i==12)
+        throw invalid_argument("不是合法月份名");
+
+    if(ds[j] == ' ') {
+        end_of_month = j + 1;
+        return i+1;
+    }
+
+    for(;j<month_name[i].size();++j)
+        if(ds[j]!=month_name[i][j])
+                break;
+
+    if(j==month_name[i].size() && ds[j] == ' ') {
+        end_of_month = j + 1;
+        return i + 1;
+    }
+
+    throw invalid_argument("不是合法月份名");
+}
+
+inline int get_day(string &ds, int month, int &p)
+{
+    size_t q;
+    int day = stoi(ds.substr(p), &q);       //从p剩下的,q是记下位置，转换的时间写入day中
+    if(day<1 || day>days[month])
+        throw invalid_argument("不是合法日期值");
+    p += q;
+    return day;
+}
+
+inline int get_year(string &ds, int &p)
+{
+    size_t q;
+    int year = stoi(ds.substr(p), &q);
+    if(p+q<ds.size())
+        throw invalid_argument("非法结尾内容");
+    return year;
+}
+
+date::date(string &ds)
+{
+    int p;
+    size_t q;
+
+    if((p = ds.find_first_of("0123456789")) == string::npos)
+        throw invalid_argument("没有数字，非法日期");
+
+    if(p>0) {
+        month = get_month(ds, p);
+        day = get_day(ds, month, p);
+        if(ds[p] != ' ' && ds[p] != ',')
+            throw invalid_argument("非法间隔符");
+        p++;
+        year = get_year(ds, p);
+    } else {
+        month = stoi(ds, &q);
+        p=q;
+        if(month < 1 || month > 12)
+            throw invalid_argument("不是合法月份值");
+        if(ds[p++] != '/')
+            throw invalid_argument("非法间隔符");
+        day = get_day(ds, month, p);
+        if(ds[p++] != '/')
+            throw invalid_argument("非法间隔符");
+        year = get_year(ds, p);
+    }
+}
+
+ostream & operator <<(ostream& out, const date& d)
+{
+    out<<d.y()<<" "<<d.m()<<" "<<d.d()<<" "<<endl;
+    return out;
+}
+
 #endif // DATA_H
+
+
