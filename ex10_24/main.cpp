138a845e7704aceff9abdf11b5d3d14f3d73f627
chentuo14 <ch122633@126.com>
2017-11-07 21:13:52 +0800

    10.24给定要给string,使用bind和check_size在一个int的vector中查找第一个大于string长度的值

---
+++ ex10_24/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+using namespace std::placeholders;
+
+string make_plural(size_t ctr,const string &word, const string &ending)
+{
+  return (ctr==1) ? word : word+ending;
+}
+
+bool check_size(const string &s, string::size_type sz)
+{
+    return s.size()<=sz;
+}
+
+void biggies(vector<int> &vc, const string &s)
+{
+    auto p = find_if(vc.begin(), vc.end(),
+                     bind(check_size, s, _1));
+    cout<<p - vc.begin()+1<<" "<<*p<<" >= "<<s<<"的长度"<<endl;
+}
+
+int main()
+{
+    vector<int> vc = {1,2,3,4,5,6,7,8,9};
+
+    biggies(vc, "Hello");
+    biggies(vc, "everyone");
+    biggies(vc, "!");
+
+    return 0;
+}
