2f6ede2dd5e4f2d248087318380ec4fcacfb9c50
chentuo <ch122633@126.com>
2017-11-08 09:51:14 +0800

    11.4扩展你的程序，忽略大小写和标点。例如,"example."、"example,"和"Example"应该递增相同的计数器

---
+++ ex11_4/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+string &trans(string &s)
+{
+    for(int p =0;p<s.size();p++) {
+        if(s[p] >='A' && s[p] <='Z')
+            s[p]=s[p]-('A'-'a');
+        else if(s[p] == ',' || s[p] == '.')
+            s.erase(p,1);
+    }
+    return s;
+}
+
+int main()
+{
+    map<string, size_t> word_count;
+    string word;
+    while(cin>>word)
+       ++word_count[trans(word)];
+
+    for(const auto &w : word_count)
+        cout<<w.first<<" occurs "<<w.second
+            <<((w.second > 1)?" times":" time")<<endl;
+    return 0;
+}
