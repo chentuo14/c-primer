8ca520c4b3127a0a27a48db8fe3fd1ffb31f2f95
chentuo <ch122633@126.com>
2017-11-08 09:34:06 +0800

    11.3编写你自己的单词计数程序

--- ex11_3/main.cpp
+++ ex11_3/main.cpp
@@ -9,6 +9,7 @@ int main()
     string word;
     while(cin>>word)
        ++word_count[word];
+
     for(const auto &w : word_count)
         cout<<w.first<<" occurs "<<w.second
             <<((w.second > 1)?" times":" time")<<endl;
