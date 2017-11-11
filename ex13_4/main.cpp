ec2caccc20afa15bac5324c987254955f4257ad5
chentuo <ch122633@126.com>
2017-11-11 09:07:47 +0800

    13.4假定Point是一个类类型，它有一个public的拷贝构造函数，指出下面的情况

---
+++ ex13_4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+//Point global;
+//Point foo_bar(Point arg)
+//{
+//    Point local = arg, *heap = new Point(global);将arg拷贝给local
+//    *heap = local;    将local拷贝到heap指定的地址中
+//    Point pa[4] = { local, *heap }; 将local和*heap拷贝给数组的前两个元素
+//    return *heap;
+//}
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
