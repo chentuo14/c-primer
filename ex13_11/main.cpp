cd7aaabe71654c5390729f00e6f1da4ce958a44b
chentuo <ch122633@126.com>
2017-11-11 09:55:25 +0800

    13.11为前面练习中的HasPtr类添加一个析构函数。

---
+++ ex13_11/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class HasPtr {
+public:
+    HasPtr(const HasPtr &hp) {
+        ps = new string(*hp.ps);
+        i = hp.i;
+    }
+
+    HasPtr(const std::string &s = std::string()):
+        ps(new std::string(s)), i(0) { }
+
+    ~HasPtr() { delete ps;}
+private:
+    std::string *ps;
+    int i;
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
